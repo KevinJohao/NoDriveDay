2f607d8e1d2adfbbbb8f23338badf277b44d625f
Johao Lema <johaolema@gmail.com>
2024-12-12 23:32:34 -0500

    Finish test with Jest

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,6 @@
 import { NoDriveDay } from '../src/models/NoDriveDay';
 
 test('Should not allow driving if it is a restricted day and time', () => {
-    expect(NoDriveDay.canDrive("PBC-1233", "2024-12-09", "08:00")).toBe("Cannot drive");
+    expect(NoDriveDay.canDrive("PBC-1231", "2024-12-9", "08:00")).toBe("Can't drive");
   });
   
